33352dd5e751366619cd59359c179c0db1ee77d0
Aamin Chaudhari <aaminchaudhari@gmail.com>
2023-05-22 18:33:51 +0530

    milestone 2

--- src/main/java/com/springrest/User_Management/controller/WalletController.java
+++ src/main/java/com/springrest/User_Management/controller/WalletController.java
@@ -15,14 +15,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 //import static org.springframework.http.converter.ResourceRegionHttpMessageConverter.print;
 
 @RestController
 public class WalletController {
 
-	public TransactionDao transactionDao;
-	public WalletDao walletDao;
-	//public UserDao userDao;
+    public TransactionDao transactionDao;
+    public WalletDao walletDao;
+    //public UserDao userDao;
     @Autowired
     public WalletService walletService;
 
@@ -31,30 +33,30 @@ public class WalletController {
         return "Welcome to the Wallet Management portal.";
     }
 
-//	@PostMapping("/wallet")
-//	public ResponseEntity <?> createORupdate(@RequestBody Wallet wallet){
-//		//System.out.println("kjdfdbfid");
-//		System.out.println(wallet);
-//		//if (!walletDao.existsByMobileNo(wallet.getMobileNo())) {
-//			//Wallet walletSaved = this.walletService.createORupdate(wallet);
-//			return new ResponseEntity<>(wallet, HttpStatus.CREATED);
-//		//}
-//		//return new ResponseEntity<>("User already exists.", HttpStatus.BAD_REQUEST);
-//	}
-
 	@PostMapping("/wallet")
-	public ResponseEntity<?> addUser(@RequestBody Wallet user) {
-		System.out.println(user);
-		//return this.userService.addUser(user);
-		return new ResponseEntity<>(user, HttpStatus.CREATED);
+	public ResponseEntity <?> createORupdate(@RequestBody Wallet wallet){
+        //Optional<Wallet> existingUser = walletDao.findBymobileNo(wallet.getMobileNo());
+		if (!walletDao.existsByMobileNo(wallet.getMobileNo())) {
+			Wallet walletSaved = this.walletService.createORupdate(wallet);
+			return new ResponseEntity<>(wallet, HttpStatus.CREATED);
+		}
+		return new ResponseEntity<>("User already exists.", HttpStatus.BAD_REQUEST);
 	}
+
 
 //	@PostMapping("/Transaction")
 //	public ResponseEntity <?> transaction(@RequestBody Transaction transaction){
-//		var x = walletDao.find
-//		if(transactionDao.existsByMobileNo(transaction.getPayee_mobileNo())&&transactionDao.existsByMobileNo(transaction.getPayer_mobileNo())&&walletDao.)
+//		//var x = walletDao.find
+//		if(transactionDao.existsBymobileNo(transaction.)&&transactionDao.existsByMobileNo(transaction.getPayer_mobileNo())&&walletDao.){
+//
+//        }
 //		Transaction transaction1 = this.walletService.maketransaction(transaction);
 //		return new ResponseEntity<Transaction>(transaction1,HttpStatus.CREATED);
 //	}
+
+    @PostMapping("/Transaction")
+    public ResponseEntity <?> transaction (@RequestBody Transaction transaction){
+        //if(!transactionDao.)
+    }
 
 }
